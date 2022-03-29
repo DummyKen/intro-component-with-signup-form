7ef49855dca2e5f95a84621ed2d00d503c182266
Ken <banyarnaingcodes@gmail.com>
2022-03-29 15:02:19 +0630

    nearly completed

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-//Form validations
-
-// select components
-const claimBtn = document.getElementById('freetrial');
-const firstName = document.getElementById('firstname');
-const lastName = document.getElementById('lastname');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-
-// svg
-const error = document.createElement('span');
-error.innerHTML = "<img src="https://svgshare.com/i/fhJ.svg">"
-
-
-// lets add event listeners
-claimBtn.addEventListener('click', () => {
-  // console.log(
-  //   `Hello! ${firstName.value} ${lastName.value}, your email address is ${email.value} and your password is ${password.value}
-  //   `
-  // );
-
-  // form validations
-  if(firstName.value = ""){
-    firstName.appendChild(error)
-  }
-})
+// //Form validations
+//
+// // select components
+// const claimBtn = document.getElementById('freetrial');
+// const firstName = document.getElementById('firstname');
+// const lastName = document.getElementById('lastname');
+// const email = document.getElementById('email');
+// const password = document.getElementById('password');
+//
+// // svg
+// const error = document.createElement('span');
+// error.innerHTML = "<img src="https://svgshare.com/i/fhJ.svg">"
+//
+//
+// // lets add event listeners
+// claimBtn.addEventListener('click', () => {
+//   // console.log(
+//   //   `Hello! ${firstName.value} ${lastName.value}, your email address is ${email.value} and your password is ${password.value}
+//   //   `
+//   // );
+//
+//   // form validations
+//  if(firstName.value === " "){
+//    firstName.classList.append('invalid')
+//    console.log("first name is empty")
+//  }
+// })
+console.log("Boo")
